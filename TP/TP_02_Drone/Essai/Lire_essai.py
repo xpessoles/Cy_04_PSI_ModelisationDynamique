8471b6404446fb800cad45a81a26c324aadf4237
Xavier Pessoles <xpessoles.ptsi@free.fr>
2017-11-20 13:26:04 +0100

    MAJ du fichier de parsing

--- TP/TP_02_Drone/Essai/Lire_essai.py
+++ TP/TP_02_Drone/Essai/Lire_essai.py
@@ -14,5 +14,12 @@ for ligne in data:
     les_t.append(float(ligne[0]))
     les_angles.append(float(ligne[1]))
     
+# Conversion des angles
+for i in range(len(les_angles)):
+    les_angles[i]=les_angles[i]*0.0055-0.11#+0.0899
+
+
 plt.plot(les_t,les_angles)
+plt.grid()
 plt.show()
+
